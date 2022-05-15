bbd30b871fa9aadc44de0bc4cc196daf53e9f373
Михаил Козин <Vasago85@mail.ru>
2022-05-15 21:59:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace lesson___3_10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)  // точка входа
+        {
+            int m;
+
+            m = int.Parse(ReadLine());
+
+            if (m % 2 == 0)           
+            { 
+               WriteLine("число чётное"); 
+            } 
+            else
+            { 
+               WriteLine("число нечётное"); 
+            }
+
+
+            ReadKey();
+        
+        }
+    }
+}
